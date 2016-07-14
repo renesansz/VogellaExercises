3d8134fd941c0785ae0608149a341c4e940d451d
Renemari Padillo <padillo.renemari@gmail.com>
2016-07-14 15:58:04 +0800

    [CHORE] Added AndroidIntents Exercise

---
+++ AndroidIntents/app/src/main/java/com/example/renesansz/androidintents/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.renesansz.androidintents;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Constant to determine which sub-activity returns
+    private static final int REQUEST_CODE = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        EditText text = (EditText)findViewById(R.id.inputForIntent);
+        String value = text.getText().toString();
+
+        Intent i = new Intent(this, ResultActivity.class);
+        i.putExtra("testValue", value);
+
+        startActivityForResult(i, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data.hasExtra("returnKey")) {
+                String result = data.getExtras().getString("returnKey");
+                if (result != null && result.length() > 0) {
+                    Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+}
---
+++ AndroidIntents/app/src/main/java/com/example/renesansz/androidintents/ResultActivity.java
@@ -0,0 +1,36 @@
+package com.example.renesansz.androidintents;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ResultActivity extends Activity {
+
+    @Override
+    public void finish() {
+        Intent intent = new Intent();
+        EditText editText = (EditText)findViewById(R.id.returnValue);
+
+        String string = editText.getText().toString();
+        intent.putExtra("returnKey", string);
+
+        setResult(RESULT_OK, intent);
+
+        super.finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Bundle extras = getIntent().getExtras();
+        String inputString = extras .getString("testValue");
+        TextView tv = (TextView)findViewById(R.id.displayIntentExtra);
+        tv.setText(inputString);
+    }
+
+}
