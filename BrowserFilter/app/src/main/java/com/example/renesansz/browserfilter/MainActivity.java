00355ee2b5b3a596eee23fcc06f3788b92f49680
Renemari Padillo <padillo.renemari@gmail.com>
2016-07-15 11:14:49 +0800

    [FIX] No default activity found for Browser Filter app

---
+++ BrowserFilter/app/src/main/java/com/example/renesansz/browserfilter/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.renesansz.browserfilter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by renesansz on 15/07/2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
