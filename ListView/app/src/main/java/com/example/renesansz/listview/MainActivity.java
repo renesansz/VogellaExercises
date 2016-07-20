6136ca12b68b3db33acb0c31cbad564bdb1097aa
Renemari Padillo <padillo.renemari@gmail.com>
2016-07-20 17:12:29 +0800

    [FEAT] Added ListView exercise app

---
+++ ListView/app/src/main/java/com/example/renesansz/listview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.renesansz.listview;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String[] values = new String[] {
+                "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu",
+                "Windows 7", "Mac OSX", "Linux", "OS/2"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String item = (String) getListAdapter().getItem(position);
+
+        Toast.makeText(this, item + " selected", Toast.LENGTH_SHORT).show();
+    }
+}
