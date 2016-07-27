9aa25f2adb1e6dacf2aedc06e58d2a8629bcf1a0
Renemari Padillo <padillo.renemari@gmail.com>
2016-07-27 13:57:56 +0800

    [CHORE] Updated MainActivity

--- AndroidToolbar/app/src/main/java/com/example/renesansz/androidtoolbar/MainActivity.java
+++ AndroidToolbar/app/src/main/java/com/example/renesansz/androidtoolbar/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,5 +38,12 @@ public class MainActivity extends AppCompatActivity {
         menuInflater.inflate(R.menu.mainmenu, menu);
 
         return true;
+    }
+
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                                        |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                                        |View.SYSTEM_UI_FLAG_FULLSCREEN
+                                                        |View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 }
