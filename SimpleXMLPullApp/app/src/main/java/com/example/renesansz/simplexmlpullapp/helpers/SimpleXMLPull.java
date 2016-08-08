fd8b067badc2d9104d726dc7306a49b09cbafbb4
Renemari Padillo <padillo.renemari@gmail.com>
2016-08-08 11:21:46 +0800

    [FEAT] Added simple XML pull app.

---
+++ SimpleXMLPullApp/app/src/main/java/com/example/renesansz/simplexmlpullapp/helpers/SimpleXMLPull.java
@@ -0,0 +1,43 @@
+package com.example.renesansz.simplexmlpullapp.helpers;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class SimpleXMLPull {
+
+    private static final String LCAT = "SimpleXML";
+
+    public static void main (String args[]) throws XmlPullParserException, IOException {
+
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+        factory.setNamespaceAware(true);
+
+        XmlPullParser xpp = factory.newPullParser();
+
+        xpp.setInput(new StringReader("<foo>Hello World!</foo>"));
+
+        int eventType = xpp.getEventType();
+
+        while(eventType != XmlPullParser.END_DOCUMENT) {
+            if(eventType == XmlPullParser.START_DOCUMENT) {
+                System.out.println("Start document");
+            } else if(eventType == XmlPullParser.END_DOCUMENT) {
+                System.out.println("End document");
+            } else if(eventType == XmlPullParser.START_TAG) {
+                System.out.println("Start tag "+xpp.getName());
+            } else if(eventType == XmlPullParser.END_TAG) {
+                System.out.println("End tag "+xpp.getName());
+            } else if(eventType == XmlPullParser.TEXT) {
+                System.out.println("Text "+xpp.getText());
+            }
+
+            eventType = xpp.next();
+        }
+    }
+
+}
